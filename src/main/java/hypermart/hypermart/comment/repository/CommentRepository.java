57bd0a6d82cb1912265fd4fef87128cd0717f32c
liveforone <yc4852@gmail.com>
2023-01-16 15:23:47 +0900

    Add Comment save, edit, delete, bulk delete logic.

--- src/main/java/hypermart/hypermart/comment/repository/CommentRepository.java
+++ src/main/java/hypermart/hypermart/comment/repository/CommentRepository.java
@@ -5,6 +5,7 @@ import hypermart.hypermart.item.model.Item;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -12,4 +13,12 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @Query("select c from Comment c join c.writer join c.item where c.item = :item")
     Page<Comment> findCommentsByItem(@Param("item") Item item, Pageable pageable);
+
+    @Modifying
+    @Query("update Comment c set c.content = :content where c.id = :id")
+    void editContent(@Param("content") String content, @Param("id") Long id);
+
+    @Modifying(clearAutomatically = true)
+    @Query("delete from Comment c where c.item = :item")
+    void deleteBulkCommentsByItem(@Param("item") Item item);
 }
--- src/main/java/hypermart/hypermart/comment/service/CommentService.java
+++ src/main/java/hypermart/hypermart/comment/service/CommentService.java
@@ -1,9 +1,11 @@
 package hypermart.hypermart.comment.service;
 
+import hypermart.hypermart.comment.dto.CommentRequest;
 import hypermart.hypermart.comment.dto.CommentResponse;
 import hypermart.hypermart.comment.repository.CommentRepository;
 import hypermart.hypermart.comment.util.CommentMapper;
 import hypermart.hypermart.item.model.Item;
+import hypermart.hypermart.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,10 +18,36 @@ import org.springframework.transaction.annotation.Transactional;
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final MemberRepository memberRepository;
 
     public Page<CommentResponse> getCommentsByItem(Item item, Pageable pageable) {
         return CommentMapper.entityToDtoPage(
                 commentRepository.findCommentsByItem(item, pageable)
         );
+    }
+
+    @Transactional
+    public void saveComment(CommentRequest commentRequest, Item item, String email) {
+        commentRequest.setItem(item);
+        commentRequest.setWriter(memberRepository.findByEmail(email));
+
+        commentRepository.save(
+                CommentMapper.dtoToEntity(commentRequest)
+        );
+    }
+
+    @Transactional
+    public void editComment(String content, Long id) {
+        commentRepository.editContent(content, id);
+    }
+
+    @Transactional
+    public void deleteCommentById(Long id) {
+        commentRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void deleteCommentsByItem(Item item) {
+        commentRepository.deleteBulkCommentsByItem(item);
     }
 }

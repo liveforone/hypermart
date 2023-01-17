e2221161ff1153f0b1cc5ef3afdc7c4d5fc1220b
liveforone <yc4852@gmail.com>
2023-01-17 18:57:53 +0900

    Refactor default policy.

--- src/main/java/hypermart/hypermart/orders/util/DiscountPolicy.java
+++ src/main/java/hypermart/hypermart/orders/util/DiscountPolicy.java
@@ -10,19 +10,19 @@ public class DiscountPolicy {
         int totalPrice = item.getPrice() * orderCount;
         MemberGrade memberGrade = member.getMemberGrade();
 
-        if (memberGrade == MemberGrade.BRONZE) {
-            return totalPrice;
-        }
-        if (memberGrade == MemberGrade.SILVER) {
-            return totalPrice * 99 / 100;
-        }
-        if (memberGrade == MemberGrade.GOLD) {
-            return totalPrice * 97 / 100;
+        if (memberGrade == MemberGrade.DIA) {
+            return totalPrice * 92 / 100;
         }
         if (memberGrade == MemberGrade.PLATINUM) {
             return totalPrice * 95 / 100;
         }
-        return totalPrice * 92 / 100;
+        if (memberGrade == MemberGrade.GOLD) {
+            return totalPrice * 97 / 100;
+        }
+        if (memberGrade == MemberGrade.SILVER) {
+            return totalPrice * 99 / 100;
+        }
+        return totalPrice;
     }
 
     public static int calculateSpecialDiscount(Item item, int orderCount) {
